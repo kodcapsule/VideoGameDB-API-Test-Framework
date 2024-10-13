c08fad9ddaf82072a43828fdf049289f4a211406
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2024-10-13 12:31:45 +0000

    addeded two test cases to user authentication controller

--- src/test/java/api/test/UserAuthTest.java
+++ src/test/java/api/test/UserAuthTest.java
@@ -12,22 +12,51 @@ import io.restassured.response.Response;
 public class UserAuthTest {
 	
 
-	User userPayload;
+	User authUserPayload;
+	User wrongAuthUserPayload;
 	
 	
 	@BeforeClass
 	public void setupData() {	
-		userPayload = new  User();
-		userPayload.setUsername("admin");
-		userPayload.setPassword("admin");
-		
+		authUserPayload = new  User();
+		authUserPayload.setUsername("admin");
+		authUserPayload.setPassword("admin");		
 	}
 	
+	
+	
+//	Test case for correct username and password
+	
 	@Test(priority=1)
-	public void testPostUser () {
-		Response response = UserEndpoints.authUser(userPayload);
+	public void testAuthUser () {
+		Response response = UserEndpoints.authUser(authUserPayload);
 		response.then().log().all();		
-		Assert.assertEquals(response.getStatusCode(),200);
+		Assert.assertEquals(response.getStatusCode(),200);		
 		Assert.assertNotNull(response.jsonPath().get("token"));
 	}
+	
+	
+	
+//	Test case for wrong username and password
+	@Test(priority=1)
+	public void testWrongAuthtUserDetails () {
+		authUserPayload.setUsername("admin101");
+		authUserPayload.setPassword("admin101");
+		Response response = UserEndpoints.authUser(authUserPayload);
+		response.then().log().all();		
+		Assert.assertEquals(response.getStatusCode(),403);		
+	}
+	
+		
+//	Test case for wrong username
+	@Test(priority=2)
+	public void testWrongAuthtUsername () {
+		authUserPayload.setUsername("admin101");
+		authUserPayload.setPassword("admin");
+		Response response = UserEndpoints.authUser(authUserPayload);
+		response.then().log().body();		
+		Assert.assertEquals(response.getStatusCode(),400);		
+	}
 }
+
+
